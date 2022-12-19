3dea1ea4505d5195fb071d8ca1cb4b7f6b7bab87
Sq1zzz <denis.isayev2003@gmail.com>
2022-12-19 21:00:49 +0500

    shh

--- ECOPACK/users/models.py
+++ ECOPACK/users/models.py
@@ -5,4 +5,4 @@ class CustomUser(AbstractUser):
     id = models.BigAutoField(primary_key=True)
     name = models.CharField(null=False,blank=True, max_length=30, verbose_name='Имя')
     surname = models.CharField(null=False,blank=True, max_length=30, verbose_name='Фамилия')
-    email = models.EmailField(blank=True, max_length=254, verbose_name='Почта')
+    email = models.EmailField(blank=True, max_length=60, verbose_name='Почта')
